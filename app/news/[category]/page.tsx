6a2aa3a9b88810a90d7a615aea8c77e23b5c08ed
iqbal sahrizal <maniacjoe3@gmail.com>
2022-12-08 16:50:11 +0700

    fitur:dynamic routing

---
+++ app/news/[category]/page.tsx
@@ -0,0 +1,22 @@
+import fetchNews from "../../../lb/fetchNews";
+import NewsList from "../../NewsList";
+import { categories } from "../../../constants";
+
+type Props = {
+  params: { category: Category };
+};
+
+async function NewsCategory({ params: { category } }: Props) {
+  const news: NewsResponse = await fetchNews(category);
+
+  return (
+    <div>
+      <h1 className="headerTitle">{category}</h1>
+      <NewsList news={news} />
+    </div>
+  );
+}
+
+export default NewsCategory;
+
+export async function generateStaticParams() {}
